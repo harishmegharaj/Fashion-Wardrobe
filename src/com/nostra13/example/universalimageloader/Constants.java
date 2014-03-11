51faac4a12d037e8d50d6c25eaee5cd63dc8e1c3
sagarinnova <sagarinnova@yahoo.co.in>
2014-03-11 10:03:24 +0530

    Fashion Wardrobe

---
+++ src/com/nostra13/example/universalimageloader/Constants.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * Copyright 2011-2013 Sagar Das
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.nostra13.example.universalimageloader;
+
+/**
+ * @author Sagar Das (nostra13[at]gmail[dot]com)
+ */
+public final class Constants {
+
+	public static final String[] IMAGES = new String[] {
+			// Heavy images
+			"http://cdn-s3-2.wanelo.com/product/image/10188916/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10188116/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10187695/x354.jpg",
+			"http://cdn-s3-0.wanelo.com/product/image/10189139/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10188840/x354.jpg",
+			"http://cdn-s3-3.wanelo.com/product/image/10082750/x354.jpg",
+			"http://cdn-s3-0.wanelo.com/product/image/10111444/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10078126/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10130215/x354.jpg",
+			"http://cdn-s3-0.wanelo.com/product/image/10088636/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10156763/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10146836/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10133535/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10163585/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10151584/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10144480/x354.jpg",
+			"http://cdn-s3-3.wanelo.com/product/image/10125239/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10153556/x354.jpg",
+			"http://cdn-s3-0.wanelo.com/product/image/10145665/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10129187/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10144327/x354.jpg",
+			"http://cdn-s3-3.wanelo.com/product/image/10132431/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10150812/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10125346/x354.jpg",
+			"http://cdn-s3-3.wanelo.com/product/image/10130244/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10162947/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10132091/x354.jpg",
+			"http://cdn-s3-3.wanelo.com/product/image/10163732/x354.jpg",
+			"http://cdn-s3-3.wanelo.com/product/image/10151151/x354.jpg",
+			"http://cdn-s3-3.wanelo.com/product/image/10130860/x354.jpg",
+			"http://cdn-s3-1.wanelo.com/product/image/10126487/x354.jpg",
+			"http://cdn-s3-0.wanelo.com/product/image/10144653/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10161247/x354.jpg",
+			"http://cdn-s3-2.wanelo.com/product/image/10125247/x354.jpg",
+			"http://cdn-s3-0.wanelo.com/product/image/10130294/x354.jpg",
+			"https://lh5.googleusercontent.com/-UGgssvFRJ7g/URquueyJzGI/AAAAAAAAAbs/yYIBlLT0toM/s1024/Into%252520the%252520Blue.jpg",
+			"https://lh3.googleusercontent.com/-CH7KoupI7uI/URquu0FF__I/AAAAAAAAAbs/R7GDmI7v_G0/s1024/Jelly%252520Fish%2525202.jpg",
+			"https://lh4.googleusercontent.com/-pwuuw6yhg8U/URquvPxR3FI/AAAAAAAAAbs/VNGk6f-tsGE/s1024/Jelly%252520Fish%2525203.jpg",
+			"https://lh5.googleusercontent.com/-GoUQVw1fnFw/URquv6xbC0I/AAAAAAAAAbs/zEUVTQQ43Zc/s1024/Kauai.jpg",
+			"https://lh6.googleusercontent.com/-8QdYYQEpYjw/URquwvdh88I/AAAAAAAAAbs/cktDy-ysfHo/s1024/Kyoto%252520Sunset.jpg",
+			"https://lh4.googleusercontent.com/-vPeekyDjOE0/URquwzJ28qI/AAAAAAAAAbs/qxcyXULsZrg/s1024/Lake%252520Tahoe%252520Colors.jpg",
+			"https://lh4.googleusercontent.com/-xBPxWpD4yxU/URquxWHk8AI/AAAAAAAAAbs/ARDPeDYPiMY/s1024/Lava%252520from%252520the%252520Sky.jpg",
+			"https://lh3.googleusercontent.com/-897VXrJB6RE/URquxxxd-5I/AAAAAAAAAbs/j-Cz4T4YvIw/s1024/Leica%25252050mm%252520Summilux.jpg",
+			"https://lh5.googleusercontent.com/-qSJ4D4iXzGo/URquyDWiJ1I/AAAAAAAAAbs/k2pBXeWehOA/s1024/Leica%25252050mm%252520Summilux.jpg",
+			"https://lh6.googleusercontent.com/-dwlPg83vzLg/URquylTVuFI/AAAAAAAAAbs/G6SyQ8b4YsI/s1024/Leica%252520M8%252520%252528Front%252529.jpg",
+			"https://lh3.googleusercontent.com/-R3_EYAyJvfk/URquzQBv8eI/AAAAAAAAAbs/b9xhpUM3pEI/s1024/Light%252520to%252520Sand.jpg",
+			"https://lh3.googleusercontent.com/-fHY5h67QPi0/URqu0Cp4J1I/AAAAAAAAAbs/0lG6m94Z6vM/s1024/Little%252520Bit%252520of%252520Paradise.jpg",
+			"https://lh5.googleusercontent.com/-TzF_LwrCnRM/URqu0RddPOI/AAAAAAAAAbs/gaj2dLiuX0s/s1024/Lone%252520Pine%252520Sunset.jpg",
+			"https://lh3.googleusercontent.com/-4HdpJ4_DXU4/URqu046dJ9I/AAAAAAAAAbs/eBOodtk2_uk/s1024/Lonely%252520Rock.jpg",
+			"https://lh6.googleusercontent.com/-erbF--z-W4s/URqu1ajSLkI/AAAAAAAAAbs/xjDCDO1INzM/s1024/Longue%252520Vue.jpg",
+			"https://lh6.googleusercontent.com/-0CXJRdJaqvc/URqu1opNZNI/AAAAAAAAAbs/PFB2oPUU7Lk/s1024/Look%252520Me%252520in%252520the%252520Eye.jpg",
+			"https://lh3.googleusercontent.com/-D_5lNxnDN6g/URqu2Tk7HVI/AAAAAAAAAbs/p0ddca9W__Y/s1024/Lost%252520in%252520a%252520Field.jpg",
+			"https://lh6.googleusercontent.com/-flsqwMrIk2Q/URqu24PcmjI/AAAAAAAAAbs/5ocIH85XofM/s1024/Marshall%252520Beach%252520Sunset.jpg",
+			"https://lh4.googleusercontent.com/-Y4lgryEVTmU/URqu28kG3gI/AAAAAAAAAbs/OjXpekqtbJ4/s1024/Mono%252520Lake%252520Blue.jpg",
+			"https://lh4.googleusercontent.com/-AaHAJPmcGYA/URqu3PIldHI/AAAAAAAAAbs/lcTqk1SIcRs/s1024/Monument%252520Valley%252520Overlook.jpg",
+			"https://lh4.googleusercontent.com/-vKxfdQ83dQA/URqu31Yq_BI/AAAAAAAAAbs/OUoGk_2AyfM/s1024/Moving%252520Rock.jpg",
+			"https://lh5.googleusercontent.com/-CG62QiPpWXg/URqu4ia4vRI/AAAAAAAAAbs/0YOdqLAlcAc/s1024/Napali%252520Coast.jpg",
+			"https://lh6.googleusercontent.com/-wdGrP5PMmJQ/URqu5PZvn7I/AAAAAAAAAbs/m0abEcdPXe4/s1024/One%252520Wheel.jpg",
+			"https://lh6.googleusercontent.com/-6WS5DoCGuOA/URqu5qx1UgI/AAAAAAAAAbs/giMw2ixPvrY/s1024/Open%252520Sky.jpg",
+			"https://lh6.googleusercontent.com/-u8EHKj8G8GQ/URqu55sM6yI/AAAAAAAAAbs/lIXX_GlTdmI/s1024/Orange%252520Sunset.jpg",
+			"https://lh6.googleusercontent.com/-74Z5qj4bTDE/URqu6LSrJrI/AAAAAAAAAbs/XzmVkw90szQ/s1024/Orchid.jpg",
+			"https://lh6.googleusercontent.com/-lEQE4h6TePE/URqu6t_lSkI/AAAAAAAAAbs/zvGYKOea_qY/s1024/Over%252520there.jpg",
+			"https://lh5.googleusercontent.com/-cauH-53JH2M/URqu66v_USI/AAAAAAAAAbs/EucwwqclfKQ/s1024/Plumes.jpg",
+			"https://lh3.googleusercontent.com/-eDLT2jHDoy4/URqu7axzkAI/AAAAAAAAAbs/iVZE-xJ7lZs/s1024/Rainbokeh.jpg",
+			"https://lh3.googleusercontent.com/-KtLJ3k858eY/URqvC_2h_bI/AAAAAAAAAbs/zzEBImwDA_g/s1024/Stream.jpg",
+			"https://lh5.googleusercontent.com/-dFB7Lad6RcA/URqvDUftwWI/AAAAAAAAAbs/BrhoUtXTN7o/s1024/Strip%252520Sunset.jpg",
+			"https://lh5.googleusercontent.com/-at6apgFiN20/URqvDyffUZI/AAAAAAAAAbs/clABCx171bE/s1024/Sunset%252520Hills.jpg",
+			"https://lh4.googleusercontent.com/-7-EHhtQthII/URqvEYTk4vI/AAAAAAAAAbs/QSJZoB3YjVg/s1024/Tenaya%252520Lake%2525202.jpg",
+			"https://lh6.googleusercontent.com/-8MrjV_a-Pok/URqvFC5repI/AAAAAAAAAbs/9inKTg9fbCE/s1024/Tenaya%252520Lake.jpg",
+			"https://lh5.googleusercontent.com/-B1HW-z4zwao/URqvFWYRwUI/AAAAAAAAAbs/8Peli53Bs8I/s1024/The%252520Cave%252520BW.jpg",
+			"https://lh3.googleusercontent.com/-PO4E-xZKAnQ/URqvGRqjYkI/AAAAAAAAAbs/42nyADFsXag/s1024/The%252520Fisherman.jpg",
+			"https://lh4.googleusercontent.com/-iLyZlzfdy7s/URqvG0YScdI/AAAAAAAAAbs/1J9eDKmkXtk/s1024/The%252520Night%252520is%252520Coming.jpg",
+			"https://lh6.googleusercontent.com/-G-k7YkkUco0/URqvHhah6fI/AAAAAAAAAbs/_taQQG7t0vo/s1024/The%252520Road.jpg",
+			"https://lh6.googleusercontent.com/-h-ALJt7kSus/URqvIThqYfI/AAAAAAAAAbs/ejiv35olWS8/s1024/Tokyo%252520Heights.jpg",
+			"https://lh5.googleusercontent.com/-Hy9k-TbS7xg/URqvIjQMOxI/AAAAAAAAAbs/RSpmmOATSkg/s1024/Tokyo%252520Highway.jpg",
+			"https://lh6.googleusercontent.com/-83oOvMb4OZs/URqvJL0T7lI/AAAAAAAAAbs/c5TECZ6RONM/s1024/Tokyo%252520Smog.jpg",
+			"https://lh3.googleusercontent.com/-FB-jfgREEfI/URqvJI3EXAI/AAAAAAAAAbs/XfyweiRF4v8/s1024/Tufa%252520at%252520Night.jpg",
+			"https://lh4.googleusercontent.com/-vngKD5Z1U8w/URqvJUCEgPI/AAAAAAAAAbs/ulxCMVcU6EU/s1024/Valley%252520Sunset.jpg",
+			"https://lh6.googleusercontent.com/-DOz5I2E2oMQ/URqvKMND1kI/AAAAAAAAAbs/Iqf0IsInleo/s1024/Windmill%252520Sunrise.jpg",
+			"https://lh5.googleusercontent.com/-biyiyWcJ9MU/URqvKculiAI/AAAAAAAAAbs/jyPsCplJOpE/s1024/Windmill.jpg",
+			"https://lh4.googleusercontent.com/-PDT167_xRdA/URqvK36mLcI/AAAAAAAAAbs/oi2ik9QseMI/s1024/Windmills.jpg",
+			"https://lh5.googleusercontent.com/-kI_QdYx7VlU/URqvLXCB6gI/AAAAAAAAAbs/N31vlZ6u89o/s1024/Yet%252520Another%252520Rockaway%252520Sunset.jpg",
+			"https://lh4.googleusercontent.com/-e9NHZ5k5MSs/URqvMIBZjtI/AAAAAAAAAbs/1fV810rDNfQ/s1024/Yosemite%252520Tree.jpg",
+			// Light images
+			"http://tabletpcssource.com/wp-content/uploads/2011/05/android-logo.png",
+			"http://simpozia.com/pages/images/stories/windows-icon.png",
+			"https://si0.twimg.com/profile_images/1135218951/gmail_profile_icon3_normal.png",
+			"http://www.krify.net/wp-content/uploads/2011/09/Macromedia_Flash_dock_icon.png",
+			"http://radiotray.sourceforge.net/radio.png",
+			"http://www.bandwidthblog.com/wp-content/uploads/2011/11/twitter-logo.png",
+			"http://weloveicons.s3.amazonaws.com/icons/100907_itunes1.png",
+			"http://weloveicons.s3.amazonaws.com/icons/100929_applications.png",
+			"http://www.idyllicmusic.com/index_files/get_apple-iphone.png",
+			"http://www.frenchrevolutionfood.com/wp-content/uploads/2009/04/Twitter-Bird.png",
+			"http://3.bp.blogspot.com/-ka5MiRGJ_S4/TdD9OoF6bmI/AAAAAAAAE8k/7ydKtptUtSg/s1600/Google_Sky%2BMaps_Android.png",
+			"http://www.desiredsoft.com/images/icon_webhosting.png",
+			"http://goodereader.com/apps/wp-content/uploads/downloads/thumbnails/2012/01/hi-256-0-99dda8c730196ab93c67f0659d5b8489abdeb977.png",
+			"http://1.bp.blogspot.com/-mlaJ4p_3rBU/TdD9OWxN8II/AAAAAAAAE8U/xyynWwr3_4Q/s1600/antivitus_free.png",
+			"http://cdn3.iconfinder.com/data/icons/transformers/computer.png",
+			"http://cdn.geekwire.com/wp-content/uploads/2011/04/firefox.png?7794fe",
+			"https://ssl.gstatic.com/android/market/com.rovio.angrybirdsseasons/hi-256-9-347dae230614238a639d21508ae492302340b2ba",
+			"http://androidblaze.com/wp-content/uploads/2011/12/tablet-pc-256x256.jpg",
+			"http://www.theblaze.com/wp-content/uploads/2011/08/Apple.png",
+			"http://1.bp.blogspot.com/-y-HQwQ4Kuu0/TdD9_iKIY7I/AAAAAAAAE88/3G4xiclDZD0/s1600/Twitter_Android.png",
+			"http://3.bp.blogspot.com/-nAf4IMJGpc8/TdD9OGNUHHI/AAAAAAAAE8E/VM9yU_lIgZ4/s1600/Adobe%2BReader_Android.png",
+			"http://cdn.geekwire.com/wp-content/uploads/2011/05/oovoo-android.png?7794fe",
+			"http://icons.iconarchive.com/icons/kocco/ndroid/128/android-market-2-icon.png",
+			"http://thecustomizewindows.com/wp-content/uploads/2011/11/Nicest-Android-Live-Wallpapers.png",
+			"http://c.wrzuta.pl/wm16596/a32f1a47002ab3a949afeb4f",
+			"http://macprovid.vo.llnwd.net/o43/hub/media/1090/6882/01_headline_Muse.jpg",
+			// Special cases
+			"http://cdn.urbanislandz.com/wp-content/uploads/2011/10/MMSposter-large.jpg", // Very large image
+			"http://4.bp.blogspot.com/-LEvwF87bbyU/Uicaskm-g6I/AAAAAAAAZ2c/V-WZZAvFg5I/s800/Pesto+Guacamole+500w+0268.jpg", // Image with "Mark has been invalidated" problem
+			"file:///sdcard/Universal Image Loader @#&=+-_.,!()~'%20.png", // Image from SD card with encoded symbols
+			"assets://Living Things @#&=+-_.,!()~'%20.jpg", // Image from assets
+			"drawable://" + R.drawable.ic_launcher, // Image from drawables
+			"http://upload.wikimedia.org/wikipedia/ru/b/b6/Как_кот_с_мышами_воевал.png", // Link with UTF-8
+			"https://www.eff.org/sites/default/files/chrome150_0.jpg", // Image from HTTPS
+			"http://bit.ly/soBiXr", // Redirect link
+			"http://img001.us.expono.com/100001/100001-1bc30-2d736f_m.jpg", // EXIF
+			"", // Empty link
+			"http://wrong.site.com/corruptedLink", // Wrong link
+	};
+
+	private Constants() {
+	}
+
+	public static class Config {
+		public static final boolean DEVELOPER_MODE = false;
+	}
+	
+	public static class Extra {
+		public static final String IMAGES = "com.nostra13.example.universalimageloader.IMAGES";
+		public static final String IMAGE_POSITION = "com.nostra13.example.universalimageloader.IMAGE_POSITION";
+	}
+}
